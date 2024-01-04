a4c4cef82af5c349f4191135e87282e0ce0e6f54
seungyoon <syoon830@gmail.com>
2024-01-04 23:09:25 +0900

    tag 페이지 추가

---
+++ pages/tags/[tag].vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import type { QueryBuilderParams } from '@nuxt/content/dist/runtime/types'
+
+const route = useRoute();
+const query: QueryBuilderParams = { path: '/posts', where: [{ 'tags': { $in: route.params.tag }, }], sort: [{ date: -1 }] }
+</script>
+
+<template>
+  <tags />
+  <ContentList :query="query" v-slot="{ list }">
+    <div v-for="post in list" :key="post._path">
+      <h2><NuxtLink :to="post._path">{{ post.title }}</NuxtLink></h2>
+    </div>
+  </ContentList>
+</template>
+
+<style scoped>
+
+</style>
