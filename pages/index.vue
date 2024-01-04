a9de6be96043d1152a5c6026e9c3d1335b177a85
seungyoon <syoon830@gmail.com>
2024-01-04 23:09:16 +0900

    content list 호출 방식 변경

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,15 @@
+<script setup lang="ts">
+import type { QueryBuilderParams } from '@nuxt/content/dist/runtime/types'
+
+const route = useRoute();
+const query: QueryBuilderParams = { path: '/posts', sort: [{ date: -1 }] }
+</script>
 <template>
   <main>
-    <ContentList path="/posts" v-slot="{ list }">
+    <tags />
+    <ContentList :query="query" v-slot="{ list }">
       <div v-for="post in list" :key="post._path">
         <h2><NuxtLink :to="post._path">{{ post.title }}</NuxtLink></h2>
-        <p>{{ post.description }}</p>
       </div>
     </ContentList>
   </main>
