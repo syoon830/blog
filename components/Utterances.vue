8b40143a8d2093d60ae6a3e758ef4665f038c492
seungyoon <syoon830@gmail.com>
2024-01-04 23:06:58 +0900

    replace issue-term value url -> pathname

--- components/Utterances.vue
+++ components/Utterances.vue
@@ -10,7 +10,7 @@ onMounted(() => {
   script.setAttribute('src', 'https://utteranc.es/client.js');
 
   script.setAttribute('repo', 'syoon830/blog'); // 여기에 GitHub 저장소 이름을 넣으세요.
-  script.setAttribute('issue-term', 'url')
+  script.setAttribute('issue-term', 'pathname')
   script.setAttribute('theme', 'github-light');
   script.setAttribute('crossOrigin', 'anonymous')
   script.setAttribute('async', true);
