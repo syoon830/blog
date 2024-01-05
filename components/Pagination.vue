8ddaf0ca84419f607189ab04680c67875da60bd5
seungyoon <syoon830@gmail.com>
2024-01-05 21:21:41 +0900

    pagination 개발

---
+++ components/Pagination.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const count = await queryContent('posts').count();
+
+const totalPages = computed(() => {
+  return Math.ceil(count / 5);
+})
+</script>
+
+<template>
+  <ul>
+    <li v-for="page in totalPages">
+      <NuxtLink :to="`/pagination/${page}`">{{ page }}</NuxtLink>
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+
+</style>
---
+++ pages/tags/[tag]/pagination/[id].vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import type {QueryBuilderParams} from "@nuxt/content/dist/runtime/types";
+const route = useRoute();
+const query: QueryBuilderParams = { path: '/posts', sort: [{ date: -1 }], limit: 5, skip: (route.params.id - 1) * 5 }
+</script>
+
+<template>
+  <tags />
+  <ContentList :query="query" v-slot="{ list }">
+    <div v-for="post in list" :key="post._path">
+      <h2><NuxtLink :to="post._path">{{ post.title }}</NuxtLink></h2>
+    </div>
+  </ContentList>
+  <pagination />
+</template>
+
+<style scoped>
+
+</style>
