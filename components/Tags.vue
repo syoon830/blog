f3d2a3816cdbb8937ff72432a349c4f80f9a9186
seungyoon <syoon830@gmail.com>
2024-01-06 13:55:25 +0900

    태그 목록 tailwind css 작업

--- components/Tags.vue
+++ components/Tags.vue
@@ -1,7 +1,9 @@
 <script setup>
+const { activeTag } = defineProps(['activeTag'])
 const data = await queryContent('posts').where({tags: { $exists: true }}).find();
 const tags = ref([]);
 const tagsSet = new Set();
+
 data.forEach(item => {
   item.tags.forEach(tag => {
     tagsSet.add(tag);
@@ -11,9 +13,11 @@ data.forEach(item => {
 tags.value = Array.from(tagsSet);
 </script>
 <template>
-    <ul>
-      <li v-for="tag in tags">
-          <NuxtLink :to="`/tags/${tag}`">{{ tag }}</NuxtLink>
-      </li>
-    </ul>
+  <div class="max-w-4xl mx-auto mt-8 p-4">
+    <div class="flex space-x-2">
+      <span v-for="tag in tags">
+        <NuxtLink :to="`/tags/${tag}`" class="bg-blue-200 text-blue-800 px-2 py-1 rounded-full text-xs [&.router-link-exact-active]:bg-blue-500 [&.router-link-exact-active]:text-white">{{ tag }}</NuxtLink>
+      </span>
+    </div>
+  </div>
 </template>
