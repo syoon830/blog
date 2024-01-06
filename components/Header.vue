5568d99a7a8be28faf6a7b068dc279cdad782580
seungyoon <syoon830@gmail.com>
2024-01-06 13:50:10 +0900

    create Header component

---
+++ components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="bg-blue-500 p-4 text-white">
+    <div class="container mx-auto flex justify-between items-center">
+      <!-- 로고 시작 -->
+      <NuxtLink to="/" class="text-2xl font-semibold">seungyoon</NuxtLink>
+      <!-- 로고 끝 -->
+    </div>
+  </header>
+</template>
+
+<style scoped>
+
+</style>
