5d7f2e14256d1a82d1498b573f2fdeb701879493
seungyoon <syoon830@gmail.com>
2024-01-22 16:36:58 +0900

    tags 디자인 변경

--- components/Tags.vue
+++ components/Tags.vue
@@ -13,10 +13,10 @@ data.forEach(item => {
 tags.value = Array.from(tagsSet);
 </script>
 <template>
-  <div class="max-w-4xl mx-auto mt-8 p-4">
-    <div class="flex space-x-2">
+  <div class="max-w-4xl mx-auto mt-8 pb-4">
+    <div class="flex">
       <span v-for="tag in tags">
-        <NuxtLink :to="`/tags/${tag}`" class="bg-blue-200 text-blue-800 px-2 py-1 rounded-full text-xs [&.router-link-exact-active]:bg-blue-500 [&.router-link-exact-active]:text-white">{{ tag }}</NuxtLink>
+        <NuxtLink :to="`/tags/${tag}`" class="text-sm flex items-center bg-blue-300 p-0 px-2 rounded-md mr-2 truncate text-white [&.router-link-exact-active]:bg-blue-500 [&.router-link-exact-active]:text-white">{{ tag }}</NuxtLink>
       </span>
     </div>
   </div>
