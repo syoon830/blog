0e0fe1570b6bf30be73f1f515d1adc93defdba9f
김승윤(프리아이브) <syoon830@freeive.com>
2024-05-24 17:48:24 +0900

    prettier 적용

--- pages/posts/[slug].vue
+++ pages/posts/[slug].vue
@@ -1,66 +1,38 @@
 <script setup lang="ts">
-  import Callout_block from "~/components/notion/callout_block.vue";
-  import Paragraph_block from "~/components/notion/paragraph_block.vue";
-  import Heading_2_block from "~/components/notion/heading_2_block.vue";
-  import Quote_block from "~/components/notion/quote_block.vue";
-  import Heading_3_block from "~/components/notion/heading_3_block.vue";
-  import Image_block from "~/components/notion/image_block.vue";
-  import Code_block from "~/components/notion/code_block.vue";
-  import Bulleted_list_item_block from "~/components/notion/bulleted_list_item_block.vue";
-  import PostViewTitle from "~/components/PostViewTitle.vue";
-  import Divider_block from "~/components/notion/divider_block.vue";
-  import Numbered_list_item_block from "~/components/notion/numbered_list_item_block.vue";
+import Callout_block from "~/components/notion/callout_block.vue";
+import Paragraph_block from "~/components/notion/paragraph_block.vue";
+import Heading_2_block from "~/components/notion/heading_2_block.vue";
+import Quote_block from "~/components/notion/quote_block.vue";
+import Heading_3_block from "~/components/notion/heading_3_block.vue";
+import Image_block from "~/components/notion/image_block.vue";
+import Code_block from "~/components/notion/code_block.vue";
+import Bulleted_list_item_block from "~/components/notion/bulleted_list_item_block.vue";
+import PostViewTitle from "~/components/PostViewTitle.vue";
+import Divider_block from "~/components/notion/divider_block.vue";
+import Numbered_list_item_block from "~/components/notion/numbered_list_item_block.vue";
 
-  definePageMeta({
-    layout: 'post-view-layout'
-  })
+definePageMeta({
+  layout: "post-view-layout",
+});
 </script>
 
 <template>
   <ContentDoc v-slot="{ doc }">
     <article>
-      <PostViewTitle :doc="doc"/>
+      <PostViewTitle :doc="doc" />
       <template v-for="child in doc.body.children">
-<!--        <p>tag: {{ child.tag }}</p>-->
-        <heading_2_block
-          v-if="child.tag === 'h2'"
-          :child="child"
-        />
-        <heading_3_block
-            v-if="child.tag === 'h3'"
-            :child="child"
-        />
-        <paragraph_block
-          v-if="child.tag === 'p'"
-          :child="child"
-        />
-        <callout_block
-          v-if="child.tag === 'aside'"
-          :child="child"
-        />
-        <quote_block
-            v-if="child.tag === 'blockquote'"
-            :child="child"
-        />
-        <image_block
-          v-if="child.tag ==='img'"
-          :child="child"
-        />
-        <code_block
-          v-if="child.tag === 'pre'"
-          :child="child"
-        />
-        <bulleted_list_item_block
-          v-if="child.tag === 'ul'"
-          :child="child"
-        />
-        <numbered_list_item_block
-            v-if="child.tag === 'ol'"
-            :child="child"
-        />
-        <divider_block
-          v-if="child.tag === 'hr'"
-        />
+        <!--        <p>tag: {{ child.tag }}</p>-->
+        <heading_2_block v-if="child.tag === 'h2'" :child="child" />
+        <heading_3_block v-if="child.tag === 'h3'" :child="child" />
+        <paragraph_block v-if="child.tag === 'p'" :child="child" />
+        <callout_block v-if="child.tag === 'aside'" :child="child" />
+        <quote_block v-if="child.tag === 'blockquote'" :child="child" />
+        <image_block v-if="child.tag === 'img'" :child="child" />
+        <code_block v-if="child.tag === 'pre'" :child="child" />
+        <bulleted_list_item_block v-if="child.tag === 'ul'" :child="child" />
+        <numbered_list_item_block v-if="child.tag === 'ol'" :child="child" />
+        <divider_block v-if="child.tag === 'hr'" />
+        <!-- {{ child }} -->
       </template>
     </article>
   </ContentDoc>
@@ -70,6 +42,4 @@
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
