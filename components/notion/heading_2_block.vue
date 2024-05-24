c7bb01b6e3c4094ec1e82696b59db94e328a752c
김승윤(프리아이브) <syoon830@freeive.com>
2024-05-24 17:41:36 +0900

    prettier 적용

--- components/notion/heading_2_block.vue
+++ components/notion/heading_2_block.vue
@@ -1,16 +1,32 @@
 <script setup>
-const { child } = defineProps(['child']);
+const { child } = defineProps(["child"]);
 </script>
 <template>
- <div style="width: 100%; margin-top: 1.4em; margin-bottom: 1px;">
-   <div style="display: flex; width: 100%; color: inherit; fill: inherit;">
-     <h3 style="max-width: 100%; width: 100%; white-space: pre-wrap; word-break: break-word; caret-color: rgb(55, 53, 47); padding: 3px 2px; font-family: ui-sans-serif, -apple-system, BlinkMacSystemFont, 'Segoe UI', Helvetica, 'Apple Color Emoji', Arial, sans-serif, 'Segoe UI Emoji', 'Segoe UI Symbol'; font-weight: 600; font-size: 1.5em; line-height: 1.3; margin: 0px;">
-       <span style="font-weight:600">
+  <div style="width: 100%; margin-top: 1.4em; margin-bottom: 1px">
+    <div style="display: flex; width: 100%; color: inherit; fill: inherit">
+      <h3
+        style="
+          max-width: 100%;
+          width: 100%;
+          white-space: pre-wrap;
+          word-break: break-word;
+          caret-color: rgb(55, 53, 47);
+          padding: 3px 2px;
+          font-family: ui-sans-serif, -apple-system, BlinkMacSystemFont,
+            'Segoe UI', Helvetica, 'Apple Color Emoji', Arial, sans-serif,
+            'Segoe UI Emoji', 'Segoe UI Symbol';
+          font-weight: 600;
+          font-size: 1.5em;
+          line-height: 1.3;
+          margin: 0px;
+        "
+      >
+        <span style="font-weight: 600">
           <template v-for="text in child.children">
-              {{ text.value }}
+            {{ text.value }}
           </template>
-       </span>
-     </h3>
-   </div>
- </div>
+        </span>
+      </h3>
+    </div>
+  </div>
 </template>
